98952e98197f9bb70f3df24312479bef559e4701
cupOWater <something2805@gmail.com>
2023-07-24 17:41:20 +0700

    Initial Commit

---
+++ Urban Legends/Urban_LegendsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Urban_LegendsApp.swift
+//  Urban Legends
+//
+//  Created by MacNCheese on 24/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct Urban_LegendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
